b2d6ce2fe97c68a9d8b2717c96cdfc1841b90c9b
tree.xie <vicansocanbico@gmail.com>
2017-07-26 23:04:56 +0800

    Add lodash module

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import defer from 'lodash/defer';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App },
+
+defer(() => {
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App },
+  });
+  console.dir(window.TIMING);
 });
 
