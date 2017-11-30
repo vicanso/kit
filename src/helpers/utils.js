2735d41bf4e9869a66bb64756946a94a3a863790
vicanso <vicansocanbico@gmail.com>
2017-11-30 20:41:53 +0800

    feat: add `detect webp` function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -7,13 +7,13 @@ import { sha256 } from '@/helpers/crypto';
 // eslint-disable-next-line
 export const setType = moduleName => mutationName => `${moduleName}/${mutationName}`;
 
-export const genPassword = (account, password) => {
+export function genPassword(account, password) {
   const pwd = sha256(password);
   const app = globals.get('CONFIG.app', 'unknown');
   return sha256(`${account}-${pwd}-${app}`);
-};
+}
 
-export const formatDate = (value) => {
+export function formatDate(value) {
   if (!value) {
     return '';
   }
@@ -26,4 +26,25 @@ export const formatDate = (value) => {
   const minutes = fill(date.getMinutes());
   const seconds = fill(date.getSeconds());
   return `${date.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-};
+}
+
+// 判断是否支持webp格式
+let checkWebpPromise = null;
+export async function isSupportWebp() {
+  if (checkWebpPromise) {
+    return checkWebpPromise;
+  }
+  const images = {
+    basic: 'data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==',
+    lossless: 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAQAAAAfQ//73v/+BiOh/AAA=',
+  };
+  const check = data => new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = resolve;
+    img.onerror = reject;
+    img.src = data;
+  });
+  checkWebpPromise = Promise.all(_.map(images, check)).then(() => true)
+    .catch(() => false);
+  return checkWebpPromise;
+}
